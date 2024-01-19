38cdf7b918683c8624ee4b2f79ca407e2608036c
jwbjnwolf <59905825+jwbjnwolf@users.noreply.github.com>
2024-01-19 15:03:35 +0000

    Compiled assets
    Source commits:
    - Re-enable layout switching in `local_settings`: https://github.com/jwbjnwolf/masto-fe-standalone/commit/1ccec998b5ed2d9135e2ea99eda12fdd9b130b58
    - Toggle `local_settings` defaults: https://github.com/jwbjnwolf/masto-fe-standalone/commit/75b2847cc5ba56e4cbba16466c1815128c22f741
    - Update source footer link: https://github.com/jwbjnwolf/masto-fe-standalone/commit/6412d94091cb01104d55050d5e797f9421f7d0be
    - Add logout button to sidebar: https://github.com/jwbjnwolf/masto-fe-standalone/commit/378180864a4b1f461b863be5c67b14417221049d
    - Remove about from sidebar: https://github.com/jwbjnwolf/masto-fe-standalone/commit/27c30fb346e91b18302075118a33ab3001f3e8ef

---
+++ public/packs/js/core/inert.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[450],{1817:function(n,o,p){n.exports=p(1867)},1867:function(n,o,p){"use strict";p.r(o)}},[[1817,0]]]);
+//# sourceMappingURL=inert.js.map
---
+++ public/packs/js/locales/vanilla/ry-json.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[728],{1031:function(p){p.exports=JSON.parse("{}")}}]);
+//# sourceMappingURL=ry-json.js.map
